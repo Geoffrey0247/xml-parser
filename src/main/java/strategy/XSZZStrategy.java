5b07aa6745bf7ce45b11e29038f7649425a33b6a
Geoffrey0247 <18912227221@163.com>
2016-12-24 23:44:10 +0800

    添加刑事质证分析策略

---
+++ src/main/java/strategy/XSZZStrategy.java
@@ -0,0 +1,12 @@
+package strategy;
+
+import org.w3c.dom.Document;
+
+/**
+ * Created by GBC on 2016/12/24.
+ */
+public class XSZZStrategy implements ZZStrategy{
+    public Document addZZ(Document xmldoc) {
+        return null;
+    }
+}
